f8024761a9ec0a74142c8e498452cd36c2eeed70
trbnkvdmr <trubnikov@taigaiot.com>
2023-12-07 19:21:21 +0700

    Update mainApp.java

--- src/main/mainApp.java
+++ src/main/mainApp.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 import main.gui.mainwindow;
 
-public class mainapp {
+public class mainApp {
 	public static void main(String[] args) throws IOException {
 		mainwindow mainwindow = new mainwindow();
 		mainwindow.createmainwindow();

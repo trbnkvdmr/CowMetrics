4f62cbe8b190f07c2ab167ee575a4db9f5895399
trbnkvdmr <trbnkvdmr@HOME-PC.local>
2023-12-11 23:08:53 +0700

    Update MainWindow.java and MainApp.java

---
+++ src/main/MainApp.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.io.IOException;
+
+import main.gui.MainWindow;
+
+/**
+ * @see url <a href="https://github.com/trbnkvdmr/CowMetrics">CowMetrics - github</a>
+ * @author Dmitry_Sergeevich
+ * @version 0.1
+ */
+public class MainApp {
+	public static void main(String[] args) throws IOException {
+		MainWindow MainWindow = new MainWindow();
+		MainWindow.CreateMainWindow();
+	}
+}
